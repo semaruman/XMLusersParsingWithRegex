f7bdc05ef1239430ca306b3f9361c34394ecb428
Student <Student@DESKTOP-JVBDPB7>
2026-05-06 21:07:15 +0300

    Добавьте файлы проекта.

---
+++ XMLparsingWithRegex/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMLparsingWithRegex.Models
+{
+    public class User
+    {
+        public string UserFIO {  get; set; } //Фамилия И.О.
+        public DateTime UserBirthDate { get; set; } //дд.мм.гггг
+        public string UserNumber { get; set; } //+7(nnn)nnn-nn-nn
+
+        public int UserRating { get; set; } //n
+    }
+}
---
+++ XMLparsingWithRegex/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+using XMLparsingWithRegex.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string xmlPath = @"XMLFile.xml";
+        string excelPath = @"ExcelFile.xlsx";
+
+        XDocument xmlDocument = XDocument.Load(xmlPath);
+
+        //получение моделей пользователей из XML документа
+        var users = xmlDocument.Root.Elements()
+            .Select(element =>
+            {
+                var elements = element.Elements().ToArray();
+                string fio = GetFio(elements[0].Value);
+                DateTime birthDate = GetBirthDate(elements[1].Value);
+                string number = GetUserNumber(elements[2].Value);
+                int rating = GetUserRating(elements[3].Value);
+
+                return new User
+                {
+                    UserFIO = fio,
+                    UserBirthDate = birthDate,
+                    UserNumber = number,
+                    UserRating = rating
+                };
+            });
+
+
+    }
+
+    
+
+    static public string GetFio(string xmlFio)
+    {
+        return "";
+    }
+
+    static public DateTime GetBirthDate(string xmlBirthDate)
+    {
+        return DateTime.Now;
+    }
+
+    static string GetUserNumber(string xmlNumber)
+    {
+        return "";
+    }
+
+    static int GetUserRating(string xmlRating)
+    {
+        return 0;
+    }
+}
