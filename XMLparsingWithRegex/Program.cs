0bc741e00cdc823058c0ec654b0d628a7919aaba
Semen <semen.ruman@gmail.com>
2026-05-09 22:27:47 +0300

    Написал функцию GetFio для преобразования строки фио из xml документа в "Фамилия И. О." при помощи регулярный выражений

--- XMLparsingWithRegex/Program.cs
+++ XMLparsingWithRegex/Program.cs
@@ -3,6 +3,7 @@
 using System.Xml.Linq;
 using System.Text.RegularExpressions;
 using XMLparsingWithRegex.Models;
+using System.Resources;
 
 public class Program
 {
@@ -12,7 +13,8 @@ public class Program
         string excelPath = @"ExcelFile.xlsx";
 
         XDocument xmlDocument = XDocument.Load(xmlPath);
-
+        Console.WriteLine("XML файл загружен!");
+        
         //получение моделей пользователей из XML документа
         var users = xmlDocument.Root.Elements()
             .Select(element =>
@@ -23,6 +25,8 @@ public class Program
                 string number = GetUserNumber(elements[2].Value);
                 int rating = GetUserRating(elements[3].Value);
 
+                //Console.WriteLine(fio);
+
                 return new User
                 {
                     UserFIO = fio,
@@ -31,15 +35,33 @@ public class Program
                     UserRating = rating
                 };
             });
-
-
+        Console.WriteLine($"Загружено пользователей: {users.Count()}");
     }
 
     
 
     static public string GetFio(string xmlFio)
     {
-        return "";
+        //Console.WriteLine($"XML фио: {xmlFio}");
+
+        string pattern = @"(?<Surname>\w+) (?<Name>\w+)[\. ]+(?<Ot>\w+\.?)";
+        var str = Regex.Match(xmlFio, pattern);
+        
+        if (str.Success)
+        {
+            string surname = str.Groups["Surname"].Value;
+            string name = str.Groups["Name"].Value;
+            string ot = str.Groups["Ot"].Value;
+
+            //формат: Фамилия И. О.
+            string res = $"{surname[0].ToString().ToUpper()}{string.Join("", surname.Skip(1))} {name[0].ToString().ToUpper()}. {ot[0].ToString().ToUpper()}.";
+
+            //Console.WriteLine($"\tИтоговое фио: {res}");
+            return res;
+        }
+
+        //Console.WriteLine("\tНеверный формат");
+        return "NULL";
     }
 
     static public DateTime GetBirthDate(string xmlBirthDate)
